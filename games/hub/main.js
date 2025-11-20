bd36d8726c68401e6052ddc617766c6877d085bb
FrancescoFattori <francescofattori04@gmail.com>
2025-11-20 17:32:06 +0100

    a

--- games/hub/main.js
+++ games/hub/main.js
@@ -3,7 +3,7 @@ async function main() {
     await initCannon("materials", true);
     await initPixi();
     //set scene
-    await scene.load("hub", root = true);
+    await scene.load("hub", true);
     player = new Player(new vec3(0,0,5)); await player.load(); scene.add(player);
     scene.play();
 }
